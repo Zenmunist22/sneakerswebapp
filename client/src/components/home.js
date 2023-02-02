c2c4c9575d0a91d7960c1e231eef78de8b4f8d35
Tommy Chang <Tommychang22@hotmail.com>
2023-02-02 15:54:47 -0500

    typescript

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
  
-export default function Home() {
+export default function Home() {sou
 
  
  // This following section will display the table with the records of individuals.
