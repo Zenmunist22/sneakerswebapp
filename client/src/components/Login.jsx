6b15d57b251a6fd592ff9226ed46593065bb1784
Luis Cabral <luisarmandocabral98@gmail.com>
2023-02-11 18:19:27 -0500

    .

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -62,7 +62,7 @@ const handleSubmit = async (event) =>{
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={user.email} onChange={handleChange}/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    <div id="emailHelp" class="form-text"></div>
   </div>
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
